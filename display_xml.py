879d0b0ec12431139786eb2c0e5c9f2f1b7f558a
M Pacer <mpacer@berkeley.edu>
2017-11-25 16:22:45 +0000

    add unique uuid to top level div element in order to ensure many styles per page

--- display_xml.py
+++ display_xml.py
@@ -2,16 +2,17 @@ from pygments import highlight
 from pygments.lexers import XmlLexer
 from pygments.formatters import HtmlFormatter
 import lxml.etree as et
+from uuid import uuid4
 
-HTML_TEMPLATE = """<style>
+HTML_TEMPLATE = """<div class={}> <style>
 {}
 </style>
 {}
+</div>
 """
 
-# html_formatter = HtmlFormatter(style=default)
 class XML:
-
+    
     def __init__(self, in_obj, style='default'):
         if isinstance(in_obj, str):
             self.text = in_obj.encode('utf-8')
@@ -23,8 +24,18 @@ class XML:
             self.text = in_obj
         self.style = style
         self.formatter = HtmlFormatter(style=self.style)
-        self.style_css = self.formatter.get_style_defs()
+        self.uuid_class = "a"+str(self.uuid)
+    
+    @property
+    def style_css(self):
+        temp_css = self.formatter.get_style_defs()
+        css_list = [f"div.{self.uuid_class} {x}" for x in temp_css.split("\n")]
+        return "\n".join(css_list)
 
+    @property
+    def uuid(self):
+        return uuid4()
+    
     def _repr_html_(self):
         content = highlight(self.text, XmlLexer(), self.formatter)
-        return HTML_TEMPLATE.format(self.style_css, content)
+        return HTML_TEMPLATE.format(self.uuid_class, self.style_css, content)
